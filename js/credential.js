ed762f78739c06fc010ed791d69523c843016456
Shirley Silvana <shirley.startary@gmail.com>
2018-02-09 01:12:12 -0600

    inicializa repositorio y enlace con firebase

---
+++ js/credential.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyAUBivIig7Es3zkPbmeLziW62tLJbjS5go",
+  authDomain: "fir-practica-298f0.firebaseapp.com",
+  databaseURL: "https://fir-practica-298f0.firebaseio.com",
+  projectId: "fir-practica-298f0",
+  storageBucket: "",
+  messagingSenderId: "621703800538"
+};
+firebase.initializeApp(config);
